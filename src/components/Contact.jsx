3d9cc857a2e255859eb3fb971644e6645ea12de6
rezahajitabar <rezahajitabarchehre1996@gmail.com>
2024-02-24 19:14:37 +0330

    create project

---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>
+        <div>
+         <input type="text" placeholder="Name"/>
+         <input type="text" placeholder="Last Name"/>
+         <input type="email" placeholder="Email"/>
+         <input type="number" placeholder="Phone"/>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
