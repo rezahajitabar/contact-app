eb2c7f9e460e131892cee014f6f20e5a0cc3c33f
rezahajitabar <rezahajitabarchehre1996@gmail.com>
2024-02-26 18:52:27 +0330

    two commit project

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,63 @@
 import React from 'react'
-
+import { useState } from 'react';
+import Contactslist from './Contactslist';
 function Contact() {
+  const [contacts,setContacts]=useState([]);
+  const [contact,setContact]=useState({
+    name: "",
+    lastName: "",
+    email: "",
+    phone: "",
+  })
+
+  const changeHandler=(event)=>{
+    const name=event.target.name;
+    const value=event.target.value;
+    setContact((contact)=> ({...contact,[name]:value}));
+  };
+
+  const addHandler=()=> {
+    setContacts((contacts) => ([...contacts,contact]));
+    setContact({
+      name: "",
+      lastName: "",
+      email: "",
+      phone: "",
+
+    })
+    console.log(contacts);
+  }
+
   return (
     <div>
         <div>
-         <input type="text" placeholder="Name"/>
-         <input type="text" placeholder="Last Name"/>
-         <input type="email" placeholder="Email"/>
-         <input type="number" placeholder="Phone"/>
+         <input type="text"
+          placeholder="Name"
+          name="name"
+           value={contact.name}
+            onChange={changeHandler}/>
+
+         <input type="text"
+          placeholder="Last Name"
+          name="lastName"
+           value={contact.lastName}
+            onChange={changeHandler}/>
+
+         <input type="email"
+          placeholder="Email"
+          name="email"
+           value={contact.email}
+            onChange={changeHandler}/>
+
+         <input type="number"
+          placeholder="Phone"
+           value={contact.phone}
+           name="phone"
+            onChange={changeHandler}/>
+
+         <button onClick={addHandler}>Add Contact</button>
         </div>
+        <Contactslist contacts={contacts}/>
     </div>
   )
 }
---
+++ src/components/Contactslist.jsx
@@ -0,0 +1,9 @@
+
+function Contactslist(props) {
+    console.log(props)
+  return (
+    <div></div>
+  )
+}
+
+export default Contactslist
